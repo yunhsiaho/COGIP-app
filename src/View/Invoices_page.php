edf65feb22850e9943bdc990a2c4f6a54169f639
hsia <yunhsia.ho@gmail.com>
2021-04-08 10:33:04 +0200

    rekha

--- src/View/Invoices_page.php
+++ src/View/Invoices_page.php
@@ -2,7 +2,7 @@
 require 'includes/header.php';
 include '../Model/Manager.php';
 include '../Model/CompaniesManager.php';
-include '../Model/InvoicesManager.php';
+include '../Model/InvoicesManager.php'; //link the manager
 
 ?>
 <div class="container">
@@ -22,7 +22,7 @@ include '../Model/InvoicesManager.php';
 		
 		$invoices= new InvoicesManager();
 
-		foreach ($invoices->getInvoices() as $invoice) { 
+		foreach ($invoices->getInvoices() as $invoice) { //to print all the invoices
 			if ($x>=20)
 			break;?>
 
@@ -32,13 +32,17 @@ include '../Model/InvoicesManager.php';
 			<td><?php echo $invoice['comp_name']?></td>
 			<form action="" method="post">
 			<td><button name="more" type="submit" value=<?php echo $invoice['comp_id']?>>more</button></td>
-			</form>
+			<!-- the button name is "more", the button value is the company id in the DB. we give every button a id so every button can go get different data-->
+			<!-- we use company id for the button id because in the query we use comp_id to connect the 2 table, 
+			so when we click on the button with a certain id, it will get the data from this line-->
+			</form> 
 			</tr>
 		<?php
 			$x++; } 
 			?>
 		</table>
 		<?php if(isset($_POST['more'])):	?>
+		<!-- when the button get clicked, it'll show all the table&data below -->
 		<table>
 		<h3>Company linked to the Invoice</h3>
 		<hr/>
@@ -47,15 +51,19 @@ include '../Model/InvoicesManager.php';
 		<th>TVA</th>
 		<th>Company Type</th>
 		</tr>
-		<?php foreach ($invoices->getDetailsComp() as $detail_Comp): ?>
+		<?php foreach ($invoices->getDetailsComp() as $detail_Comp): ?> 
+		<!-- here $invoices= new InvoicesManager(see line23), and we put variable $invoices into method getDetailsComp() -->
 			<?php if($_POST['more']==$detail_Comp['comp_id']):	?>
+			<!-- if the clicked button's id equals to one of the id in the DB, it'll show this line of the data below-->
 		<tr>
 		<td><?php echo $detail_Comp['comp_name']?></td>
 		<td><?php echo $detail_Comp['comp_VAT']?></td>
 		<td><?php echo $detail_Comp['comp_type']?></td>
 		</tr>
 		<?php break; endif?>
+		<!-- end of the filter if-->
 		<?php endforeach  ?>
+		<!-- end of the getdetailcomp foreach-->
 		</table>
 		<table>
 		<h3>Contact Person</h3>
@@ -74,6 +82,7 @@ include '../Model/InvoicesManager.php';
 		<?php endforeach  ?>
 		</table>
 		<?php endif ?>
+		<!-- end of the isset if -->
 	<?php require 'includes/footer.php'?>
 </div>       
 
