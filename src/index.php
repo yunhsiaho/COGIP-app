b7d599166a0cdde8d9cf3b1451a0c02f45a7a9b5
hsia <yunhsia.ho@gmail.com>
2021-04-19 17:24:50 +0200

    hello

--- src/index.php
+++ src/index.php
@@ -1,47 +1,41 @@
 <?php
 
-declare(strict_types=1);
+// declare(strict_types=1);
 
-require_once('Controller/HomepageController.php');
-require_once('Controller/InvoicesController.php');
-require_once('Controller/CompanyController.php');
-require_once('Controller/ContactsController.php');
-require_once('Controller/LoginController.php');
-require_once('Controller/AddCompany.php');
-require_once('Controller/AddContact.php');
-require_once('Controller/AddInvoice.php');
-//require_once('controller/RegisterController.php');
-//require_once('Controller/LoginController.php');
+// require_once('Controller/HomepageController.php');
+// require_once('Controller/InvoicesController.php');
+// require_once('Controller/CompanyController.php');
+// require_once('Controller/ContactsController.php');
+// require_once('Controller/LoginController.php');
+// require_once('Controller/AddCompany.php');
+// require_once('Controller/AddContact.php');
+// require_once('Controller/AddInvoice.php');
 
-$controller = new HomepageController();
 
-// if (isset($_GET['page']) && $_GET['page'] === 'register') {
-//     $controller = new RegisterController();
+// $controller = new HomepageController();
+
+// if (isset($_GET['page']) && $_GET['page'] === 'invoices') {
+//     $controller = new InvoicesController();
 // }
-// if (isset($_GET['page']) && $_GET['page'] === 'login') {
+// if (isset($_GET['page']) && $_GET['page'] === 'companies') {
+//     $controller = new CompanyController();
+// }
+// if (isset($_GET['page']) && $_GET['page'] === 'contacts') {
+//     $controller = new ContactController();
+// }
+// if (isset($_GET['page']) && $_GET['page'] === 'admin') {
 //     $controller = new LoginController();
 // }
+// if (isset($_GET['page']) && $_GET['page'] === 'addcontact') {
+//     $controller = new AddContact();
+// }
+// if (isset($_GET['page']) && $_GET['page'] === 'addcompany') {
+//     $controller = new AddCompany();
+// }
+// if (isset($_GET['page']) && $_GET['page'] === 'addinvoice') {
+//     $controller = new AddInvoice();
+// }
 
-if (isset($_GET['page']) && $_GET['page'] === 'invoices') {
-    $controller = new InvoicesController();
-}
-if (isset($_GET['page']) && $_GET['page'] === 'companies') {
-    $controller = new CompanyController();
-}
-if (isset($_GET['page']) && $_GET['page'] === 'contacts') {
-    $controller = new ContactController();
-}
-if (isset($_GET['page']) && $_GET['page'] === 'admin') {
-    $controller = new LoginController();
-}
-if (isset($_GET['page']) && $_GET['page'] === 'addcontact') {
-    $controller = new AddContact();
-}
-if (isset($_GET['page']) && $_GET['page'] === 'addcompany') {
-    $controller = new AddCompany();
-}
-if (isset($_GET['page']) && $_GET['page'] === 'addinvoice') {
-    $controller = new AddInvoice();
-}
-
-$controller->render();
+// $controller->render();
+?>
+<h1>hello</h1>
